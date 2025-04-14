7958b3f38eff7057637a2a43fbdd1f13db2da3f7
laurent bernabé <laurent.bernabe@gmail.com>
2025-04-15 00:17:24 +0200

    checking that the generated position is not finished

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:chess/chess.dart';
 import 'package:flutter/material.dart';
 import './question_page.dart';
 import '../logic/position_generation.dart';
@@ -5,7 +6,13 @@ import '../logic/position_generation.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  void _goToQuestionPage(BuildContext context) {
+
+  void _goToQuestionPage(BuildContext context) { 
+    String startPositionFen;
+    do {
+      startPositionFen = generatePosition();
+    } while(_isGameFinishedFor(startPositionFen));
+
     Navigator.of(context).push(
       MaterialPageRoute(
         builder: (context) {
@@ -13,6 +20,11 @@ class HomePage extends StatelessWidget {
         },
       ),
     );
+  }
+
+  bool _isGameFinishedFor(String startPositionFen) {
+    final chessLogic = Chess.fromFEN(startPositionFen);
+    return chessLogic.game_over;
   }
 
   @override
