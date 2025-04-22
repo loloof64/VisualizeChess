91f45c2667a87d1f0628436b42de4f7040ed1c6c
laurent bernabé <laurent.bernabe@gmail.com>
2025-04-22 23:39:07 +0200

    bump version

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -17,7 +17,7 @@ pluginManagement {
 }
 
 plugins {
-    id("dev.flutter.flutter-plugin-loader") version "1.0.5"
+    id("dev.flutter.flutter-plugin-loader") version "1.0.6"
     id("com.android.application") version "8.7.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.22" apply false
 }
